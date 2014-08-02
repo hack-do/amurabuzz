049b2837a0014613be5e67c02b3df9ab9bae3b6e
Shaunak Pagnis <shaunak.pagnis@gmail.com>
2014-08-03 02:24:49 +0530

    some changes in signup

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 	u1 = User.create(email: "vineet@amuratech.com",password: "qwerty007",user_name: "vinzeee", :password_confirmation => "qwerty007")
-	u2 = User.create(email: "vinzee93@gmail.com",password: "qwerty007",user_name: "vinzee", :password_confirmation => "qwerty007")
+	u2 = User.create(email: "shaunak.pagnis@gmail.com",password: "12345678",user_name: "shaunakp", :password_confirmation => "12345678")
 	u3 = User.create(email: "sayali@amuratech.com",password: "sayali123",user_name: "sayali912", :password_confirmation => "sayali123")
-	u4 = User.create(email: "shaunak@amuratech.com",password: "qwerty007",user_name: "shaunak", :password_confirmation => "qwerty007")
+	u4 = User.create(email: "shaunak@amuratech.com",password: "12345678",user_name: "shaunakpp", :password_confirmation => "12345678")
 
 	Relationship.create(follower_id: 1,followed_id: 4)
 	Relationship.create(follower_id: 1,followed_id: 2)
