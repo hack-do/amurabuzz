b87ed49c20431598a5c4ad4870698a8b118aad71
shaunakpp <shaunak.pagnis@gmail.com>
2015-05-08 12:55:41 +0530

    added migration script to travis

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,6 @@ require 'rspec/rails'
 require 'capybara/rspec'
 require 'capybara/rails'
 require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -22,7 +21,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
-
+CodeClimate::TestReporter.start
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
